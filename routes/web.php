444caf46386ff7748bf108097ca9c88344d233be
MohamedAdelEid <01114979112mohamed@gmail.com>
2024-11-14 01:14:02 +0200

    feat( make layouts dashboard )  feat( make localization using js )  feat ( create files for rtl , ltr css )

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+/*
+    |--------------------------------------------------------------------------
+    | DASHBOARD ROUTES
+    |--------------------------------------------------------------------------
+    | These routes handle all the DASHBOARD views and functionalities accessible
+    | by users or guests. They manage the user interface, site navigation, and
+    | public-facing content.
+    |--------------------------------------------------------------------------
+*/
+
+Route::group(['prefix' => 'admin'], function () {
+
+    // ------------------------------ DASHBOARD ROUTE ------------------------------ //
+    Route::get('/dashboard', function () {
+        return view('dashboard.index');
+    });
+});
